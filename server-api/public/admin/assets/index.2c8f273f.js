aca989551cf40d647d7ce3972a029f856eb38435
frank <frank@temp.com>
2023-12-16 17:58:21 +0800

    更新index

---
+++ server-api/public/admin/assets/index.2c8f273f.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./LayoutBody.7a794797.js";import{aF as _,ak as c,aA as e}from"./index.30624dae.js";const t={};function a(r,s){const o=n;return e(),c(o)}const m=_(t,[["render",a]]);export{m as default};
