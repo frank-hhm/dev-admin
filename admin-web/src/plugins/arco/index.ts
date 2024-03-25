86586870a9d42a79e1f8cd6e666a683523801ef9
frank <frank_hhm@163.com>
2024-03-25 15:33:39 +0800

    修复

--- admin-web/src/plugins/arco/index.ts
+++ admin-web/src/plugins/arco/index.ts
@@ -1,7 +1,7 @@
 import { type App } from "vue"
 import ArcoVue from '@arco-design/web-vue';
 // import '@arco-design/web-vue/dist/arco.less';
-import '@arco-themes/vue-dev-frank/index.less';
+import '@arco-themes/vue-frank-green/index.less';
 
 export function loadArcoPlus(app: App) {
   app.use(ArcoVue)
